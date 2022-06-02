0313a5e9a7d22c7838da942cc5b3ea7b84c4946e
MaksimTemnDev <79454370+MaksimTemnDev@users.noreply.github.com>
2022-06-02 19:20:00 +0300

    Files corrected to doxygen

--- changeOperandsStrs.cpp
+++ changeOperandsStrs.cpp
@@ -1,3 +1,5 @@
+/* \file
+*/
 #define _CRT_SECURE_NO_WARNINGS
 
 #include "string.h"
@@ -12,43 +14,43 @@ void changeOperandsStrs(struct Accoplishment* accomplishment)
 	unsigned short int len2 = accomplishment->secondOperandStringsCount;
 	char str[10][10];
 	unsigned short int j = 0;
-	//в случае когда первый операнд меньше
+	//!в случае когда первый операнд меньше
 	if (len1 <= len2)
 	{
 		for (unsigned short int i = 0; i < len1; ++i)
 		{
-			//скопировать первый операнд в буфферную строку
+			//!скопировать первый операнд в буфферную строку
 			strcpy(str[i], accomplishment->operandOne[i]);
 			j = i;
 		}
 		for (unsigned short int i = 0; i < len2; ++i)
 		{
-			//заменить первый операнд на второй
+			//!заменить первый операнд на второй
 			strcpy(accomplishment->operandOne[i], accomplishment->operandTwo[i]);
-			//заменить второй операнд на значение из буффера
+			//!заменить второй операнд на значение из буффера
 			if (i <= j)
 				strcpy(accomplishment->operandTwo[i], str[i]);
 		}
 	}
-	//в случае когда второй операнд меньше
+	//!в случае когда второй операнд меньше
 	else
 	{
 		for (unsigned short int i = 0; i < len2; ++i)
 		{
-			//скопировать второй операнд в буфферную строку
+			//!скопировать второй операнд в буфферную строку
 			strcpy(str[i], accomplishment->operandTwo[i]);
 			j = i;
 		}
 		for (unsigned short int i = 0; i < len1; ++i)
 		{
-			//заменить второй операнд на первый
+			//!заменить второй операнд на первый
 			strcpy(accomplishment->operandTwo[i], accomplishment->operandOne[i]);
-			//заменить перый операнд на значение из буффера
+			//!заменить перый операнд на значение из буффера
 			if (i <= j)
 				strcpy(accomplishment->operandOne[i], str[i]);
 		}
 	}
-	//поменять данные оь длинах операндов
+	//!поменять данные об длинах операндов
 	accomplishment->firstOperandStringsCount = len2;
 	accomplishment->secondOperandStringsCount = len1;
 }
