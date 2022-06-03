5f5db6f3283b07b098994d4d1ef8d5eda815d52e
MaksimTemnDev <79454370+MaksimTemnDev@users.noreply.github.com>
2022-06-03 19:23:16 +0300

    Some more cpp files correted to doxygen, and comments changed

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+/* \file
+*/
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <fstream>
@@ -12,7 +14,7 @@ using namespace std;
 // a<=b, !(a>b), !(b<a)
 // a>b, !(a>b), b<a
 
-/*Обеспечить считывание из файла, вывод в консоль ошибок, если они есть, запись ответа в выходной файл, вызов главной функции, решающей задачу.*/
+/*!Обеспечить считывание из файла, вывод в консоль ошибок, если они есть, запись ответа в выходной файл, вызов главной функции, решающей задачу.*/
 
 int main(int argc, char* argv[]) {
 	setlocale(LC_ALL, "Ru");
@@ -21,30 +23,25 @@ int main(int argc, char* argv[]) {
 	int operationType = -1;
 	struct Accoplishment accomplish;
 	char* output; 
-	//char s1[] = "C://Users//user//source//repos//KNPO_6//Debug//in.txt";
-	//char s2[] = "output";
-	//output = s2;
 	int i = 0;
 
 	try {
-		//Выдать ошибку, если входной файл не указан в аргументах командной строки
+		//!Выдать ошибку, если входной файл не указан в аргументах командной строки
 		if (argc < 2)
-			throw ("Unnable to open source file");
+			throw ("Unable to open source file");
 		else if (argc < 3)
 			throw("There is no output file name");
-		//Выдать ошибку, если входной файл невозможно открыть
+		//!Выдать ошибку, если входной файл невозможно открыть
 		fstream input;
 		char* in = argv[1]; //C:\Users\user\source\repos\KNPO_6\Debug\in.txt
 		output = argv[2]; //C:\Users\user\source\repos\KNPO_6\Debug\output.txt
-		//char* in = s1;
-		//char* output  = s2;
 		input.open(in, fstream::in);
 		if (input.is_open() == 0)
 			throw("Can not open source file");
 		while(!input.eof() && i<10)
 		{
 			input >> strMas[i];
-			//Проверить корректность входных данных.
+			//!Проверить корректность входных данных.
 			errorSearch(strMas[i]);
 			if (strstr(strMas[i], "<") != NULL || strstr(strMas[i], ">") != NULL || strstr(strMas[i], "=") != NULL)
 				i += 10;
@@ -53,14 +50,14 @@ int main(int argc, char* argv[]) {
 		}
 		if (strCount < 3)
 			throw("error, too few lines");
-	//Определить тип измененяемого выражения.
+	//!Определить тип измененяемого выражения.
 	operationType = findOperation(strMas, strCount);
-	//проверка на наличие неопознанных операций
+	//!Проверка на наличие неопознанных операций
 	if (operationType == -1)
 		throw("wrong operation type");
 	else
 	{
-		// выбор из рассматриваемых значений для записи в промежеточное значение
+		//!Выбор из рассматриваемых значений для записи в промежеточное значение
 		switch (operationType)
 		{
 		case 1:
@@ -78,22 +75,21 @@ int main(int argc, char* argv[]) {
 		default:
 			break;
 		}
-		//Записать исходные операторы.
+		//!Записать исходные операторы.
 		operandsCopy(&accomplish, strMas, strCount);
 	}
 	}
-	//Если есть ошибка
+	//!Если есть ошибка
 	catch (const char* exception)
 	{
-		//Распечатать её в консоль ошибок
+		//!Распечатать её в консоль ошибок
 		cout << exception;
-		//Завершить работу программы
+		//!Завершить работу программы
 		return 0;
 	}
-	//Изменить исходное дерево…
+	//!Изменить исходное дерево…
 	treeChange(&accomplish);
-	//Записать массив строк в выходной файл...
+	//!Записать массив строк в выходной файл...
 	saveTreeChanges(&accomplish, output);
-	//getchar();
 	return 0;
 }
--- operandsCopy.cpp
+++ operandsCopy.cpp
@@ -1,3 +1,5 @@
+/* \file
+*/
 #define _CRT_SECURE_NO_WARNINGS
 
 #include "string.h"
@@ -9,7 +11,7 @@ using namespace std;
 
 void operandsCopy(struct Accoplishment* accomplishment, char(&strMas)[10][10], int strCount)
 {
-	//Записать исходные операторы
+	//!Записать исходные операторы
 	short unsigned int arg_count = 0;
 	short unsigned int arg_secCu = 0;
 	short unsigned int action_secCu = 0;
@@ -19,56 +21,56 @@ void operandsCopy(struct Accoplishment* accomplishment, char(&strMas)[10][10], i
 	char str[30] = { "\0" };
 	char* precopy = str;
 	char* pointer = res;
-	//Для каждого элемента массива строк
+	//!Для каждого элемента массива строк
 	for (short unsigned int i = 0; i < strCount; ++i)
 	{
-		//если строка содержит чилсла или литеры
+		//!если строка содержит чилсла или литеры
 		if (isalnum(*strMas[i]))
 		{
-			//увеличим счетчик аргументов
+			//!увеличим счетчик аргументов
 			arg_count += 1;
 		}
 		else
 		{
-			//увеличим счетчик опреаций
+			//!увеличим счетчик опреаций
 			action_count += 1;
 		}
-		//если есть признак что пройденые строки форммруют операнд
+		//!если есть признак что пройденые строки форммруют операнд
 		if ((action_count == arg_count - 1 && (strCount == 3 && i == 0)) || (i > 1 && action_count >= arg_count - 1 && strCount > 3))
 		{
-			//запомним положение разделения между операндами
+			//!запомним положение разделения между операндами
 			delimiter = i;
-			//проведем проверку на наличие второго операнда
+			//!проведем проверку на наличие второго операнда
 			for (short unsigned int j = delimiter; j < strCount - 1; ++j)
 			{
 				if (isalnum(*strMas[j]))
 				{
-					//увеличим счетчик аргументов
+					//!увеличим счетчик аргументов
 					arg_secCu += 1;
 				}
 				else
 				{
-					//увеличим счетчик опреаций
+					//!увеличим счетчик опреаций
 					action_secCu += 1;
 				}
 			}
-			//если найдены операнды выйти из цикла
+			//!если найдены операнды выйти из цикла
 			if (action_secCu >= arg_secCu - 1)
 				i = strCount;
 		}
 	}
-	//записать первый операнд в заданную форму данных
+	//!записать первый операнд в заданную форму данных
 	strncpy(pointer, strMas[0], strlen(strMas[0]));
 	for (short unsigned int i = 0; i <= delimiter; ++i)
 	{
 		strcpy(accomplishment->operandOne[i], strMas[i]);
 	}
-	//записать второй операнд в заданную форму данных
+	//!записать второй операнд в заданную форму данных
 	for (short unsigned int i = delimiter + 1; i < strCount - 1; ++i)
 	{
 		strcpy(accomplishment->operandTwo[i - delimiter - 1], strMas[i]);
 	}
-	//записать данные о количестве строк в целом и в каждом опернде
+	//!записать данные о количестве строк в целом и в каждом опернде
 	accomplishment->firstOperandStringsCount = delimiter + 1;
 	accomplishment->secondOperandStringsCount = strCount - 1 - accomplishment->firstOperandStringsCount;
 	accomplishment->stringsCount = strCount;
